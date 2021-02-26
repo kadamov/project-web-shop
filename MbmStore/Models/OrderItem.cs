6fd84c74246d2559143e31f0ba85157b0daf1129
kadamov <42800170+kadamov@users.noreply.github.com>
2021-02-26 10:59:20 +0100

    OrderItem and Invoice View

--- MbmStore/Models/OrderItem.cs
+++ MbmStore/Models/OrderItem.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace MbmStore.Models
 {
-    public class OrderItem 
+    public class OrderItem : Product
     {
-        
+        //properties
+        public int OrderItemId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product;
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; }
+
+
+        //constructor
+        public OrderItem()
+        {
+
+        }
+        public OrderItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
     }
 }
