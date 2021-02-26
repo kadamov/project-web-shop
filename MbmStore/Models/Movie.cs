c46acf1f1091b2dd9f227a769f69d6fc0d269950
kadamov <42800170+kadamov@users.noreply.github.com>
2021-02-26 09:23:49 +0100

    main files
    Main files to work on for the project

---
+++ MbmStore/Models/Movie.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MbmStore.Models
+{
+    public class Movie
+    {
+        // fields
+        private decimal price;
+
+        // properties
+        public string Title { get; }
+
+        public string ImageFileName {
+            set; get;
+        }
+
+        public string Director { get; private set; }
+
+        public decimal Price
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Price is not accepted");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+            get { return price; }
+        }
+
+
+        // constructors
+        public Movie() { }
+
+        public Movie(string title, decimal price)
+        {
+            Title = title;
+            this.price = price;
+        }
+
+        public Movie(string title, decimal price, string imageFileName, string director)
+        {
+            Title = title;
+            this.price = price;
+            ImageFileName = imageFileName;
+            Director = director;
+        }
+    }
+}
---
+++ MbmStore/Models/MusicCD.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MbmStore.Models
+{
+    public class MusicCD : Product
+    {
+        //fields
+        public List<Track> Tracks { get; } = new List<Track>();
+        //properties
+        public string Artist { get; }
+        public string Label { get; set; }
+        public short Released { get; }
+
+        //methods
+        public void AddTrack(Track track)
+        {
+            Tracks.Add(track);
+        }
+
+        public TimeSpan GetPlayingTime()
+        {
+
+            var totalLength = new TimeSpan();
+            foreach (var track in Tracks)
+            {   
+                totalLength += track.Length;
+            }
+            return totalLength;
+        }
+
+        public MusicCD()
+        {
+
+        }
+        public MusicCD(string artist, string title, decimal price, short released) : base(title, price)
+        {
+            Artist = artist;
+            Title = title;
+            Price = price;
+            Released = released;
+        }
+    }
+}
